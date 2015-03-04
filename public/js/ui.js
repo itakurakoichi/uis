fa2f4e776f6e7f50646da14bcf324b459874dec2
itakurakoichi <itakura_koichi_gn@cyberagent.co.jp>
2015-03-05 04:11:23 +0900

    add toggleClass ui

---
+++ public/js/ui.js
@@ -0,0 +1,30 @@
+(function(w) {
+	"use strict";
+
+	var _ui = {
+		toggleClass: function(opts) {
+			console.log('### opts: ', opts)
+			opts = opts || {};
+			var btn       = opts.btn || [];
+			var target    = opts.target || '';
+			var className = opts.className || '';
+
+			if (btn.length > 0) {
+				btn = btn.join(',')
+			} else {
+				return;
+			}
+
+			$(btn).on('click', function() {
+				$(target).toggleClass(className);
+			});
+		}
+	}
+
+	_ui.toggleClass({
+		btn: ['.header__nav-btn', '.nav__close-btn'],
+		target: '.nav',
+		className: 'is-open'
+	});
+
+})(this);
